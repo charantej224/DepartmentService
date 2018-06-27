f384e731da46944714ca9fb30083d95bff770422
US\cthota <cthota@deloitte.com>
2018-06-27 16:40:06 +0530

    changes with Eureka

--- DepartmentService/src/main/java/com/charan/department/DepartmentServiceApplication.java
+++ DepartmentService/src/main/java/com/charan/department/DepartmentServiceApplication.java
@@ -2,10 +2,12 @@ package com.charan.department;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.charan.*")
+@EnableEurekaClient
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
